55a06d54fb2150c55d3400a77758d923673d41ca
Rob O'Gara <rgo594@gmail.com>
2019-03-14 22:04:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
-
-def countdown
+def countdown_with_sleep(y)
+  sleep(y)
 end
+
+def countdown(x)
+  counter = x + 1
+  while counter > 0
+    counter -= 1
+    puts "#{counter} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+end
+
