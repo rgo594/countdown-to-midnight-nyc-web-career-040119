390e0831b67de96ea88c80f1bbc0bdacc28f0324
Rob O'Gara <rgo594@gmail.com>
2019-03-14 22:16:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ end
 def countdown(x)
   puts "#{x} SECOND(S)!"
   counter = x
-  while counter > 0
+  while counter > 1
     counter -= 1
     puts "#{counter} SECOND(S)!"
     countdown_with_sleep(1)
